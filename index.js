4d54ce14d97bc7f00c4ef353c5bccaecc1e99943
Aaron <ztoplin@yahoo.com>
2022-09-21 13:02:41 -0400

    commit tree 62e3a1f9d9840d972cd1e6c5e517369993b5ae02

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const nondestructivelyUpdatedEmployee = { ...employee};
+    nondestructivelyUpdatedEmployee[key] = value
+    return nondestructivelyUpdatedEmployee;
+}; 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const trimmedEmployee = {...employee};
+    delete trimmedEmployee[key];
+    return trimmedEmployee 
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee; 
+};
+
+
+
